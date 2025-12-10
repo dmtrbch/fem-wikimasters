633fdf26d984489aadff57893621c90e8c4b1c4d
dmtrbch <bochvarovskidimitar@gmail.com>
2025-12-10 13:30:09 +0100

    prepare for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
+import assert from "node:assert";
 import { dirname } from "node:path";
 import type { NextConfig } from "next";
 
+assert(process.env.BLOB_BASE_URL, "you must have defined BLOB_BASE_URL");
+
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     remotePatterns: [new URL(`${process.env.BLOB_BASE_URL}/**`)],
   },
